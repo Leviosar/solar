47309a98a7efedb39c865a8cbaf330c6f3298edb
Joao Vitor Maia <maia.toString@gmail.com>
2019-12-23 20:31:04 -0300

    Initial commit

---
+++ src/js/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    let width = window.innerWidth
+    let height = window.innerHeight
+    let canvas = createCanvas(min(width, height), min(width, height))
+    canvas.parent('canvas-container')
+    background(0)
+}
+
+function draw() {}
